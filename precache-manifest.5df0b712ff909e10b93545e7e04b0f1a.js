e6adeb69c156de7bb967c5551fdabf4042417b11
rizkyari <rizkyari4121@gmail.com>
2019-11-15 16:17:05 +0700

    Updates

---
+++ precache-manifest.5df0b712ff909e10b93545e7e04b0f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c863afa42c3a49a25755af53bf4b127",
+    "url": "/about-me/index.html"
+  },
+  {
+    "revision": "d3359383000f26cd2c88",
+    "url": "/about-me/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "46a5c03ae490ed6bbd3e",
+    "url": "/about-me/static/js/2.78b03fac.chunk.js"
+  },
+  {
+    "revision": "d3359383000f26cd2c88",
+    "url": "/about-me/static/js/main.4d12a218.chunk.js"
+  },
+  {
+    "revision": "3c294a760b557155bc6f",
+    "url": "/about-me/static/js/runtime-main.f12b9810.js"
+  },
+  {
+    "revision": "b13558ff47213dfe8f6240cb71b49fb8",
+    "url": "/about-me/static/media/profile.b13558ff.jpg"
+  }
+]);
